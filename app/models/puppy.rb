9564af16122e900568da7dbca9e5865bd7497bba
sjones4215 <bluesfansince2013@gmail.com>
2020-08-28 16:41:10 -0500

    working search !

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -2,8 +2,9 @@ class Puppy < ApplicationRecord
     has_many :reviews, dependent: :destroy
     
     def self.search(search)
-      where("age LIKE ?", "%#{search}%") 
-      where("breed LIKE ?", "%#{search}%")
+      where("age ILIKE ?", "%#{search}%") 
+      where("breed ILIKE ?", "%#{search}%")
+      where("size ILIKE ?", "%#{search}%")
     end
     
 end
