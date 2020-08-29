73aefa32baa144a60a79417728a804a7b727de56
sjones4215 <bluesfansince2013@gmail.com>
2020-08-29 17:16:22 -0500

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_182913) do
+ActiveRecord::Schema.define(version: 2020_08_27_153830) do
 
   create_table "puppies", force: :cascade do |t|
     t.string "name"
